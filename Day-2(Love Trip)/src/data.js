8fa40cca662fc857df8cad1e9639b9e6a9fb8a9a
KuldeepKdy <kuldeepkdy4@gmail.com>
2023-12-25 17:47:28 +0530

    Trip with love

---
+++ Day-2(Love Trip)/src/data.js
@@ -0,0 +1,59 @@
+const data = [
+  {
+    id: 1,
+    name: "Agra",
+    info: "Agra is a city in northern India, known for the iconic Taj Mahal,",
+    image: "https://www.welcomenri.com/Tourism/img/tourism1.jpg",
+    price: "35,758",
+  },
+  {
+    id: 2,
+    name: "Jaipur",
+    info: "shut the fuck up i dont want to write anything any more",
+    image:
+      "https://www.tripsavvy.com/thmb/_wgidcLzUefiil4U1xNgv-YG18E=/3865x2576/filters:no_upscale():max_bytes(150000):strip_icc()/facade-of-the-hawa-mahal-palace-of-winds-in-jaipur-rajasthan-india-636950806-58e24eed5f9b58ef7e556bbb.jpg",
+    price: "46,458",
+  },
+  {
+    id: 3,
+    name: "Goa",
+    info: "Goa is a small state on the western coast, known for its stunning",
+    image:
+      "https://images.thrillophilia.com/image/upload/s--EhWH2JRb--/c_fill,f_auto,fl_strip_profile,g_auto,h_600,q_auto,w_975/v1/images/photos/000/115/737/original/1516998283_0_139c88_4f6e83d3_orig.jpg.jpg?1516998283",
+    price: "44,248",
+  },
+  {
+    id: 4,
+    name: "Varanasi",
+    info: "This is varanasi mujhe nhi pta kha h ye or na he m pta krne me thoda sa b intrested is a city in northern India, known for the iconic Taj Mahal,",
+    image:
+      "https://www.meganotravels.com/wp-content/uploads/2016/03/IMG_0498.jpg",
+    price: "24,525",
+  },
+  {
+    id: 5,
+    name: "Darjeeling",
+    info: "I am in darjeeling now is a city in northern India, known for the iconic Taj Mahal,",
+    image:
+      "https://th.bing.com/th/id/OIP.OndRHyu95uxFNyeaGVXGdAHaE9?rs=1&pid=ImgDetMain",
+    price: "79,732",
+  },
+  {
+    id: 6,
+    name: "Jaisalmer",
+    info: "I am in Jaisalmer now is a city in northern India, known for the iconic Taj Mahal,",
+    image:
+      "https://www.tripsavvy.com/thmb/S5sXee7ixAAm-aj3_xBtAQsPi-s=/2123x1412/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-533638374-5b9ced74c9e77c0050b73539.jpg",
+    price: "66,255",
+  },
+  {
+    id: 7,
+    name: "Kochi",
+    info: "I am in Kochi now is a city in northern India, known for the iconic Taj Mahal,",
+    image:
+      "https://www.ekeralatourism.net/wp-content/uploads/2018/12/kochi3.jpg",
+    price: "54,762",
+  },
+];
+
+export default data;
