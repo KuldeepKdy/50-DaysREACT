3768ef303fffa167129aa4585ddb941caa29a239
KuldeepKdy <kuldeepkdy4@gmail.com>
2024-01-08 12:30:35 +0530

    Testimonials Slider

---
+++ Day-4(Testimonial Slider)/src/data.jsx
@@ -0,0 +1,50 @@
+export const reviews = [
+  {
+    id: 1,
+    name: "Pranay Gupta",
+    job: "Software Engineer",
+    image:
+      "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fG1hbnxlbnwwfHwwfHx8MA%3D%3D",
+    text: "You need to add some text in there to see how it looks in the browser is cool and all but you're looking for something more realistic to how the div will be used in real life... ",
+  },
+  {
+    id: 2,
+    name: "Yash Kumar",
+    job: "Software Engineer",
+    image:
+      "https://images.unsplash.com/photo-1591084728795-1149f32d9866?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTZ8fG1hbnxlbnwwfHwwfHx8MA%3D%3D",
+    text: " It is so easy to overestimate the importance of one defining moment and underestimate the value of making small improvements on a daily basis. ",
+  },
+  {
+    id: 3,
+    name: "Vipin Yadav",
+    job: "Software Engineer",
+    image:
+      "https://images.unsplash.com/photo-1541577141970-eebc83ebe30e?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTh8fG1hbnxlbnwwfHwwfHx8MA%3D%3D",
+    text: " It doesn’t matter how successful or unsuccessful you are right now. What matters is whether your habits are putting you on the path toward success. ",
+  },
+  {
+    id: 4,
+    name: "Morish Chauhan",
+    job: "Software Engineer",
+    image:
+      "https://plus.unsplash.com/premium_photo-1675129779582-d84b954f2397?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTd8fG1hbnxlbnwwfHwwfHx8MA%3D%3D",
+    text: "If you’re having trouble changing your habits, the problem isn’t you. The problem is your system. Bad habits repeat themselves again and again not because you don’t want to change, but because you have the wrong system for change. ",
+  },
+  {
+    id: 5,
+    name: "Mehul Yadav",
+    job: "Software Engineer",
+    image:
+      "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjJ8fG1hbnxlbnwwfHwwfHx8MA%3D%3D",
+    text: "The key to building lasting habits is focusing on creating a new identity first. Your current behaviors are simply a reflection of your current identity. What you do now is a mirror image of the type of person you believe that you are (either consciously or subconsciously). ",
+  },
+  {
+    id: 6,
+    name: "Chapri Rao",
+    job: "Software Engineer",
+    image:
+      "https://images.unsplash.com/photo-1567515004624-219c11d31f2e?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjN8fG1hbnxlbnwwfHwwfHx8MA%3D%3D",
+    text: " The cue triggers a craving, which motivates a response, which provides a reward, which satisfies the craving and, ultimately, becomes associated with the cue. Together, these four steps form a neurological feedback loop—cue, craving, response, reward; cue, craving, response, reward—that ultimately allows you to create automatic habits. This cycle is known as the habit loop",
+  },
+];
