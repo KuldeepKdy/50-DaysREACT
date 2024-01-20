5c78f82faf2a1aaeadc2e409eaf38b61000ea5d5
KuldeepKdy <kuldeepkdy4@gmail.com>
2024-01-20 12:25:09 +0530

    Docs Mini App

--- Day-16(Docs Mini App)/src/components/Foreground.jsx
+++ Day-16(Docs Mini App)/src/components/Foreground.jsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import Card from "./Card";
 
 const Foreground = () => {
+  const ref = useRef(null);
+
+  const data = [
+    {
+      desc: "Lorem ipsum dolar sit amet consectetur adipisicing",
+      filesize: ".9mb",
+      close: false,
+      tag: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "blue",
+      },
+    },
+    {
+      desc: "Lorem ipsum dolar sit amet consectetur adipisicing",
+      filesize: ".9mb",
+      close: false,
+      tag: {
+        isOpen: true,
+        tagTitle: "Upload",
+        tagColor: "green",
+      },
+    },
+    {
+      desc: "Lorem ipsum dolar sit amet consectetur adipisicing",
+      filesize: ".9mb",
+      close: false,
+      tag: {
+        isOpen: false,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+  ];
   return (
-    <div className="fixed top-0 left-0 z-[3] w-full h-full">
-      <Card />
+    <div
+      ref={ref}
+      className="fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5"
+    >
+      {data.map((item, index) => (
+        <Card key={index} data={item} reference={ref} />
+      ))}
     </div>
   );
 };
