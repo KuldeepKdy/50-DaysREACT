db11e4ad64ef683aee0313b72a38fa0bab083e80
KuldeepKdy <kuldeepkdy4@gmail.com>
2024-01-23 15:43:26 +0530

    contact

--- Day-17(PortFolio)/src/components/Experiance.jsx
+++ Day-17(PortFolio)/src/components/Experiance.jsx
@@ -61,8 +61,8 @@ const Experiance = () => {
   ];
   return (
     <div
-      name="experiance "
-      className="bg-gradient-to-b  from-gray-800 to-black w-full h-screen"
+      name="experiance"
+      className="bg-gradient-to-b  from-gray-800 to-black w-full h-screen "
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
