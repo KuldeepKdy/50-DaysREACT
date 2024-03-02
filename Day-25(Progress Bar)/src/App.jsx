e2b8609cc4bc39c9dc790bcd3db3194e5728f739
KuldeepKdy <kuldeepkdy4@gmail.com>
2024-03-02 11:52:08 +0530

    Progress bar

--- Day-25(Progress Bar)/src/App.jsx
+++ Day-25(Progress Bar)/src/App.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProgressBar from "./components/ProgressBar";
 
 const App = () => {
+  const [value, setValue] = useState(0);
+  const [sucess, setSucess] = useState(false);
+  useEffect(() => {
+    setInterval(() => {
+      setValue((val) => val + 1);
+    }, 100);
+  }, []);
+
   return (
-    <div>
+    <div className="w-full p-10 h-screen gap-5 flex flex-col items-center justify-center">
       <span>Progress Bar</span>
-      <ProgressBar value={25} />
+      <ProgressBar value={value} onComplete={() => setSucess(true)} />
+      <span>{sucess ? "Complete!" : "Loading..."}</span>
     </div>
   );
 };
--- Day-25(Progress Bar)/src/components/ProgressBar.jsx
+++ Day-25(Progress Bar)/src/components/ProgressBar.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const ProgressBar = ({ value = 0 }) => {
+const ProgressBar = ({ value = 0, onComplete = () => {} }) => {
+  const [percent, setPercent] = useState(value);
+  useEffect(() => {
+    // edge case for less than 0 and more than 100
+    setPercent(Math.min(100, Math.max(value, 0)));
+
+    if (value >= 100) {
+      onComplete();
+    }
+  }, [value]);
+
   return (
-    <div className="progress">
-      <span>{value.toFixed()}%</span>
+    <div className="progress relative overflow-hidden relative flex bg-slate-200 w-[500px] h-[35px] border border-zinc-400 rounded-full">
+      {/* tofixed is used to round the input value for eg-50.2 is converted to 50 */}
+      <span
+        style={{ color: percent > 49 ? "white" : "black" }}
+        className="absolute text-white w-full flex justify-center items-center"
+      >
+        {percent.toFixed()}%
+      </span>
+      <div
+        role="progressbar"
+        aria-valuemin={0}
+        aria-valuemax={100}
+        aria-valuenow={percent.toFixed()}
+        className="h-full bg-black "
+        style={{ width: `${percent}%` }}
+      ></div>
     </div>
   );
 };
